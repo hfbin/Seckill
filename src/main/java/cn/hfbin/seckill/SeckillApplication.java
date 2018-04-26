18969de5b91a94c1ddfdf558b466dc073dfd573b
hfbin <1716850375@qq.com>
2018-04-26 11:38:42 +0800

    init

---
+++ src/main/java/cn/hfbin/seckill/SeckillApplication.java
@@ -0,0 +1,12 @@
+package cn.hfbin.seckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeckillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeckillApplication.class, args);
+	}
+}
