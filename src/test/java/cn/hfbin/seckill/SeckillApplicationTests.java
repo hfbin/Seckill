b84652343abaef194b2a1e4045784781b38f3081
hfbin <1716850375@qq.com>
2018-04-26 20:57:41 +0800

    init

--- src/test/java/cn/hfbin/seckill/SeckillApplicationTests.java
+++ src/test/java/cn/hfbin/seckill/SeckillApplicationTests.java
@@ -2,15 +2,28 @@ package cn.hfbin.seckill;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SeckillApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	@Autowired
+	DataSource dataSource;
 
+	@Test
+	public void contextLoads() throws SQLException {
+		//org.apache.tomcat.jdbc.pool.DataSource
+		System.out.println(dataSource.getClass());
+		Connection connection = dataSource.getConnection();
+		System.out.println(connection);
+		connection.close();
+
+	}
 }
