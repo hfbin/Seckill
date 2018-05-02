f18eaadfcf34abc77009bed75f9616579b8a1a08
hfbin <1716850375@qq.com>
2018-05-02 14:46:29 +0800

    init

---
+++ src/main/java/cn/hfbin/seckill/mapper/test.java
@@ -0,0 +1,16 @@
+package cn.hfbin.seckill.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * My Blog : www.hfbin.cn
+ * github: https://github.com/hfbin
+ * Created by: HuangFuBin
+ * Date: 2018/5/2
+ * Time: 14:45
+ * Such description:
+ */
+@Mapper
+public class test {
+
+}
