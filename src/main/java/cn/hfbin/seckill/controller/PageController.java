01465bd69e38da5c2fe7192f287d55c3a6ad21d4
hfbin <1716850375@qq.com>
2018-07-09 22:41:44 +0800

    PageClntroller

--- src/main/java/cn/hfbin/seckill/controller/PageController.java
+++ src/main/java/cn/hfbin/seckill/controller/PageController.java
@@ -1,10 +1,21 @@
 package cn.hfbin.seckill.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by: HuangFuBin
  * Date: 2018/7/9
  * Time: 12:36
  * Such description:
  */
+
+@Controller
 public class PageController {
+
+
+    @RequestMapping("login")
+    public String loginPage(){
+        return "login";
+    }
 }
